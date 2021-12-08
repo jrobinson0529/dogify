ac28165e6925e2db96a466a65595a85c07573df0
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-12-07 20:29:09 -0600

    scaffold backend code

---
+++ dogify.api/dogify.api/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dogify.api.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string messageText { get; set; }
+        public DateTime messageTime { get; set; }
+
+    }
+}
